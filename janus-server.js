e9413a02ab5194cb768a0f7034f06b918d7bfc5d
Will Koffel <will@koffel.org>
2016-05-12 15:11:29 +0000

    basic message handling now working

--- janus-server.js
+++ janus-server.js
@@ -1,5 +1,10 @@
 require('dotenv').config();
 
+var gpio = require("pi-gpio");
+
+var door0_pin = 16; // Pi GPIO23
+var door1_pin = 18; // Pi GPIO24
+
 var AWS = require('aws-sdk');
 var accessKey = process.env.AWS_ACCESS_KEY;
 var secretKey = process.env.AWS_SECRET_KEY;
@@ -7,6 +12,13 @@ AWS.config.update({accessKeyId: accessKey, secretAccessKey: secretKey});
 var s3 = new AWS.S3({apiVersion: '2006-03-01'});
 
 const fs = require('fs');
+
+var spawn = require('child_process').spawn;
+var proc;
+
+// launch a raspistill process to continuously capture the current image
+var args = ["-n", "-w", "640", "-h", "480", "-hf", "-vf", "-o", "/tmp/garage-image.jpg", "-t", "0", "-tl", "500"];
+proc = spawn('raspistill', args);
 
 var pubnub = require("pubnub")({
     ssl           : true,  // <- enable TLS Tunneling over TCP
@@ -36,9 +48,27 @@ function uploadNewImage() {
 function notifyNewImage(imageKey) {
   pubnub.publish({
       channel   : 'image_ready',
-      message   : {"url" : imageKey},
+      message   : {"url" : imageKey, "ts" : Date.now()},
       error     : function(e) { console.log( "Failed to send image_ready notification.", e ); }
   });
+}
+
+function pushGarageButton(door) {
+    console.log("toggle garage door ", door);
+    var door_pin = door0_pin;
+    if(door > 0) {
+      door_pin = door1_pin;
+    }
+
+    gpio.open(door_pin, "output pullup", function() {
+      gpio.write(door_pin, 0);
+      console.log("on");
+      setTimeout(function() {
+        console.log("off");
+        gpio.write(door_pin, 1);
+        gpio.close(door_pin);
+      }, 1000);
+    });
 }
 
 /* ---------------------------------------------------------------------------
@@ -49,5 +79,13 @@ pubnub.subscribe({
     callback : function(message) {
       console.log( " > ", message );
       uploadNewImage();
+    }
+});
+
+pubnub.subscribe({
+    channel  : "door_button",
+    callback : function(message) {
+      console.log( " > ", message );
+      pushGarageButton(parseInt(message.door));
     }
 });
